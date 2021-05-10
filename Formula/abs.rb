97c631806fa4185bd9d6503774bf2e5df8e9b8ee
Adrian Ho <the.gromgit@gmail.com>
2021-05-10 21:28:49 +0800

    abs: update 2.4.2 bottle.

--- Formula/abs.rb
+++ Formula/abs.rb
@@ -11,9 +11,10 @@ class Abs < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-dev/releases/download/abs-2.4.2"
-    sha256 cellar: :any_skip_relocation, big_sur:  "f1906e183d869e285800a6b94bf0de3e2b8159ceb94caaf069ce3b024709e6ba"
-    sha256 cellar: :any_skip_relocation, catalina: "2429e8a1b3afd594263a2cca87f7e644969d34668b64dc4a5216d4994e09f4cc"
-    sha256 cellar: :any_skip_relocation, mojave:   "359d840382bd91da6bb679ac92d0287579eb95eed52f1f30d68cb4577e285da4"
+    sha256 cellar: :any_skip_relocation, big_sur:      "f1906e183d869e285800a6b94bf0de3e2b8159ceb94caaf069ce3b024709e6ba"
+    sha256 cellar: :any_skip_relocation, catalina:     "2429e8a1b3afd594263a2cca87f7e644969d34668b64dc4a5216d4994e09f4cc"
+    sha256 cellar: :any_skip_relocation, mojave:       "359d840382bd91da6bb679ac92d0287579eb95eed52f1f30d68cb4577e285da4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b3683c235c178329a5ae523aff23bc93ea084af2953648015161c5db3b13f4a9"
   end
 
   head do
