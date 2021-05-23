087da3a42ecb7aa5182fa4d4d680e98e154aae64
Adrian Ho <the.gromgit@gmail.com>
2021-05-24 00:31:42 +0800

    vlang-weekly: update 2021.20 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -12,6 +12,7 @@ class VlangWeekly < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2021.20"
+    sha256 cellar: :any_skip_relocation, big_sur:      "97fab640b72f1056111ed93ad58924ce2d14b722e139e19c62b7c458e93d107d"
     sha256 cellar: :any_skip_relocation, catalina:     "2a12636cac992150c95d72534c421f7e1e5c37004051510cfea4ca3b61328b76"
     sha256 cellar: :any_skip_relocation, mojave:       "61e0dcc0036642079d4019bde0972f7848d4b9e20b75a8fbc32212cd64456bce"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "b4d653ceaf86c03d0f34e0566f3b968a982c9598d6398dc6ff88ff4f35978766"
