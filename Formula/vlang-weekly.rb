1a4959be18a2e5fa2d65d5278535016285e0fcba
Adrian Ho <aho+vm-catalina@03s.net>
2021-11-26 13:47:39 +0800

    vlang-weekly: update 2021.47 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -14,6 +14,7 @@ class VlangWeekly < Formula
     root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2021.47"
     sha256 cellar: :any_skip_relocation, monterey: "326139efdb04f39babdcafc1d59c8841cc083a3e92828f50e6c02c2057b20073"
     sha256 cellar: :any_skip_relocation, big_sur:  "4f0a9eda9fe11b3a67b79fbd1d7599ec76fbc3e26c21a62151ebb88d3572e25f"
+    sha256 cellar: :any_skip_relocation, catalina: "2c81427c02f28bff8f2fd21ea882571f2afdcaaec3e3215fbdf4f251f765ef9f"
   end
 
   conflicts_with "vlang", because: "both install `v` binaries"
