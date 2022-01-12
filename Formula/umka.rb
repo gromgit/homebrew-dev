26cfb3460c23b32cf027e528ee31856da80a495d
Adrian Ho <aho+bruteforce@03s.net>
2022-01-12 18:41:20 +0800

    umka: update 0.6 bottle.

--- Formula/umka.rb
+++ Formula/umka.rb
@@ -1,17 +1,13 @@
 class Umka < Formula
   desc "Statically-typed embeddable scripting language"
   homepage "https://github.com/vtereshkov/umka-lang"
-  url "https://github.com/vtereshkov/umka-lang/archive/v0.5.1.tar.gz"
-  sha256 "27795e8f51338907677cba6304893080ec0aaeb511401573376a73aef81892c5"
+  url "https://github.com/vtereshkov/umka-lang/archive/v0.6.tar.gz"
+  sha256 "81d7640f35c6a1c74a1553fb1af2ecf637fb7894c69919e95d66ce7a13d5e5a3"
   license "BSD-2-Clause"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-dev/releases/download/umka-0.5.1"
-    sha256 cellar: :any,                 arm64_monterey: "f57dc1a1c7abd40abfa238efbedb57894067505879e8daf639513e9dfb6af63f"
-    sha256 cellar: :any,                 big_sur:        "879cff40ab61b057faa81b46d9e0e1d4b9c44d0ea9bff79f40e7dbbee5893c5e"
-    sha256 cellar: :any,                 catalina:       "fe1f4867cebfa9347374512aa905fd4382d9fda0452e06016e16fc2637281a6b"
-    sha256 cellar: :any,                 mojave:         "1347537cb1aa3ee33eb431f6e0d5fb3a9b0123d3ded9db58d2dd7d1f46b0a7b4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7dcc5d23be0391a925c38c5cf03d0dfca5e152d58c7437a2e8ce56011e349087"
+    root_url "https://github.com/gromgit/homebrew-dev/releases/download/umka-0.6"
+    sha256 cellar: :any, arm64_monterey: "70b550127dc14661e52597eb93c70d7662b6cf6ffd5bb2d1e161afc40fbedf0f"
   end
   head do
     url "https://github.com/gromgit/umka-lang.git"
