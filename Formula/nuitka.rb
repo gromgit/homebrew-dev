df2b72525272cff705feaf7f196db206681fdfda
Adrian Ho <aho+vm-catalina@03s.net>
2022-03-07 20:46:07 +0800

    nuitka: update 0.7.3 bottle.

--- Formula/nuitka.rb
+++ Formula/nuitka.rb
@@ -12,6 +12,7 @@ class Nuitka < Formula
     sha256 cellar: :any_skip_relocation, arm64_monterey: "428ecf3e986b230949324c21befe61b041081954d8792b642c618d9f3cb69b94"
     sha256 cellar: :any_skip_relocation, monterey:       "a6905db6066b25463f3a0f87c708896cd91a4ca02a9177e5083480956fa00eb4"
     sha256 cellar: :any_skip_relocation, big_sur:        "39b8de69b7f57648a6393bda83c76cb54670242850947b9307e953c420c46937"
+    sha256 cellar: :any_skip_relocation, catalina:       "a57863348ad8743188263d5290aaa3a4d69a7008bc96650baf979c154eb28e03"
   end
 
   depends_on "llvm"
