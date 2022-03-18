7ed3cc567c30b7f99549e53d7570c5a543ee3cad
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-18 12:59:32 +0800

    vlang-weekly: update 2022.11 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -16,6 +16,7 @@ class VlangWeekly < Formula
     sha256 cellar: :any_skip_relocation, monterey:       "d6fa311326534d290f5668abbbfb78189cc0c4229278605754535d8647e6782d"
     sha256 cellar: :any_skip_relocation, big_sur:        "f1831a7115e6e6bf7953ef7e3b1f3a82bfe3823175fa9b418232e5a220893211"
     sha256 cellar: :any_skip_relocation, catalina:       "855d12477bc64829960200af0217cc16b635c63e94850b709235eb7646cc4ee8"
+    sha256 cellar: :any_skip_relocation, mojave:         "5ec0138918d37878b63f0124e9b4e12c4f5a8fc71ddbce16d74cf2cbed4014ed"
   end
 
   conflicts_with "vlang", because: "both install `v` binaries"
