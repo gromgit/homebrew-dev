2a6e500e5b3504b709c5ae7f5618419266932137
Adrian Ho <aho+vm-bigsur@03s.net>
2022-04-18 23:20:08 +0800

    vlang-weekly: update 2022.16 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -14,6 +14,7 @@ class VlangWeekly < Formula
     root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2022.16"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3163158e5ac88266b4d38824ffcf4bc110a3d674304dae2baacb5ccc8980e8d0"
     sha256 cellar: :any_skip_relocation, monterey:       "89fdf844339713877e253e9b589bb4fcbee108d64f915a84b9203359295933a9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f6a987793b99133ed4ec5dbcc23695fef832ca5723f5f04fbce8e632fca708f2"
   end
 
   conflicts_with "vlang", because: "both install `v` binaries"
