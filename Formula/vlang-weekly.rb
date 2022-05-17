278efbef879f4a5f657517ef0f83700ba9653788
Adrian Ho <aho+bruteforce@03s.net>
2022-05-17 18:21:02 +0800

    vlang-weekly: update 2022.20 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -1,8 +1,8 @@
 class VlangWeekly < Formula
   desc "V programming language"
   homepage "https://vlang.io"
-  url "https://github.com/vlang/v/archive/weekly.2022.19.tar.gz"
-  sha256 "a32c983f7d35b24fc84f76eeac75f33351c86ed3aed12b1d39c8c33b8b255e40"
+  url "https://github.com/vlang/v/archive/weekly.2022.20.tar.gz"
+  sha256 "af398163c0490e5839b9f3ee904fc57a95412b98afa3fe15b3e652e08c8db566"
   license "MIT"
 
   livecheck do
@@ -11,9 +11,8 @@ class VlangWeekly < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2022.19"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "366000c76c1ca6ced5d7fee94bc661a2dacbf70b852b99922e4d18cb6b8008b6"
-    sha256 cellar: :any_skip_relocation, mojave:         "4ff92f25b825773bd3e35814e3c890d6d9982f1f8c6d7c556fc685b6f01a660c"
+    root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2022.20"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "554cadf97baa35b9dfbe2bf76665c23af2dced8a2b4e8eca2bc8418123f93ffe"
   end
 
   conflicts_with "vlang", because: "both install `v` binaries"
