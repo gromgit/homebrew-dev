2b249efc16964164147bf033138650f64f23bcbc
Adrian Ho <the.gromgit@gmail.com>
2022-07-14 22:31:55 +0800

    umka: update 0.7 bottle.

--- Formula/umka.rb
+++ Formula/umka.rb
@@ -1,17 +1,13 @@
 class Umka < Formula
   desc "Statically-typed embeddable scripting language"
   homepage "https://github.com/vtereshkov/umka-lang"
-  url "https://github.com/vtereshkov/umka-lang/archive/v0.6.tar.gz"
-  sha256 "81d7640f35c6a1c74a1553fb1af2ecf637fb7894c69919e95d66ce7a13d5e5a3"
+  url "https://github.com/vtereshkov/umka-lang/archive/v0.7.tar.gz"
+  sha256 "2ff79437862b38711273ca10d551b86219e71fce907844214f230d3f4a5e4b00"
   license "BSD-2-Clause"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-dev/releases/download/umka-0.6"
-    sha256 cellar: :any, arm64_monterey: "70b550127dc14661e52597eb93c70d7662b6cf6ffd5bb2d1e161afc40fbedf0f"
-    sha256 cellar: :any, monterey:       "6d3a847fd1d0661e8b7451889dce1b5c79383d2793c04422fe4fada9c81b5786"
-    sha256 cellar: :any, big_sur:        "d36ef93c96040979b4b4d7d6d925615793bdec56fb9c6cb43425c8a4678da345"
-    sha256 cellar: :any, catalina:       "166f3692ce8bf93546e3401c0e428094018aeaef0ed54616b8e77f5b093c4b61"
-    sha256 cellar: :any, mojave:         "8fde8002026436e3fadb5d801d4c6d9ddc87594e6eef476bb8c4f08345c80960"
+    root_url "https://github.com/gromgit/homebrew-dev/releases/download/umka-0.7"
+    sha256 cellar: :any, arm64_monterey: "b4ee0ec1e4f4a7c194702a5b658b9b89af86f75be30314f37888871ed88ea803"
   end
   head do
     url "https://github.com/gromgit/umka-lang.git"
