e2afe6d9966d6d6a0515f34a7ef2b96158c3b2c1
Adrian Ho <aho+vm-bigsur@03s.net>
2022-07-21 16:23:31 +0800

    vlang-weekly: update 2022.29 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -12,6 +12,7 @@ class VlangWeekly < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2022.29"
+    sha256 cellar: :any_skip_relocation, monterey:     "cdac25ebacd0ad009de76b687663a8a13044aecef272ab90454ba9e045b696b0"
     sha256 cellar: :any_skip_relocation, big_sur:      "fb937a18f520cef09d5715c4800ab78131f5e145b97558949baa653ef8e65945"
     sha256 cellar: :any_skip_relocation, catalina:     "67e8ff56f35155d21af40186bed4161bc26fd958857ddc2f09641ae21ab8c382"
     sha256 cellar: :any_skip_relocation, mojave:       "7776a59d5c7c2c3b9a0ca6658f999647f3b341af94655bf40edddb5e306119bd"
