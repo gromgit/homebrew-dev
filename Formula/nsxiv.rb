346c1b12c2f9ee5a16a9fe5100f41b8e8f39ceda
Adrian Ho <the.gromgit@gmail.com>
2022-08-22 23:20:25 +0800

    [NEW] nsxiv

---
+++ Formula/nsxiv.rb
@@ -0,0 +1,31 @@
+class Nsxiv < Formula
+  desc "Neo Simple X Image Viewer"
+  homepage "https://nsxiv.codeberg.page/"
+  url "https://codeberg.org/nsxiv/nsxiv/archive/v30.tar.gz"
+  sha256 "a916d1385872ccf0b55fbf6b8546d05fcbbbb8b0a92579494e64c6bd22fc7941"
+  license "GPL-2.0-or-later"
+  head "https://codeberg.org/nsxiv/nsxiv.git", branch: "master"
+
+  depends_on "giflib"
+  depends_on "imlib2"
+  depends_on "libexif"
+  depends_on "libx11"
+  depends_on "libxft"
+  on_linux do
+    depends_on "inotify-tools"
+  end
+
+  def install
+    make_args = [
+      "PREFIX=#{prefix}",
+      "CPPFLAGS=-I#{Formula["freetype2"].opt_include}/freetype2",
+      "LDLIBS=-lpthread",
+    ]
+    make_args << "AUTORELOAD=nop" if OS.mac?
+    system "make", *make_args, "install"
+  end
+
+  test do
+    assert_match "Error opening X display", shell_output("DISPLAY= #{bin}/nsxiv #{test_fixtures("test.png")} 2>&1", 1)
+  end
+end
