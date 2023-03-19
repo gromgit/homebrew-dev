48597fb1119c3752cbdf84d353b346c65c11bce1
Adrian Ho <aho+bruteforce@03s.net>
2023-03-20 00:26:01 +0800

    codon-llvm: add 2023.09.23 bottle.

---
+++ Formula/codon-llvm.rb
@@ -0,0 +1,66 @@
+class CodonLlvm < Formula
+  desc "Custom LLVM required to build Codon"
+  homepage "https://github.com/exaloop/llvm-project"
+  url "https://github.com/exaloop/llvm-project.git",
+      tag:      "codon",
+      revision: "55b0b8fa1c9f9082b535628fc9fa6313280c0b9a"
+  version "2023.09.23"
+  license "BUSL-1.1"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-dev/releases/download/codon-llvm-2023.09.23"
+    sha256 cellar: :any, arm64_monterey: "ac65652ef686553ed1a5d63d1576a576576c99edaafcd483f587f8eb58f2b516"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "python@3.11" => :build
+
+  def install
+    args = %w[
+      -DCMAKE_BUILD_TYPE=Release
+      -DLLVM_INCLUDE_TESTS=OFF
+      -DLLVM_ENABLE_RTTI=ON
+      -DLLVM_ENABLE_ZLIB=OFF
+      -DLLVM_ENABLE_TERMINFO=OFF
+      -DLLVM_TARGETS_TO_BUILD=all
+    ]
+    system "cmake", "-S", "llvm", "-B", "build", "-G", "Ninja",
+                    *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        To use the bundled libc++ please add the following LDFLAGS:
+          LDFLAGS="-L#{opt_lib}/c++ -Wl,-rpath,#{opt_lib}/c++"
+      EOS
+    end
+  end
+
+  test do
+    llvm_version = Version.new(Utils.safe_popen_read(bin/"llvm-config", "--version").strip)
+    soversion = llvm_version.major.to_s
+    soversion << "git" if head?
+
+    assert_equal prefix.to_s, shell_output("#{bin}/llvm-config --prefix").chomp
+
+    # Ensure LLVM did not regress output of `llvm-config --system-libs` which for a time
+    # was known to output incorrect linker flags; e.g., `-llibxml2.tbd` instead of `-lxml2`.
+    # On the other hand, note that a fully qualified path to `dylib` or `tbd` is OK, e.g.,
+    # `/usr/local/lib/libxml2.tbd` or `/usr/local/lib/libxml2.dylib`.
+    shell_output("#{bin}/llvm-config --system-libs").chomp.strip.split.each do |lib|
+      if lib.start_with?("-l")
+        assert !lib.end_with?(".tbd"), "expected abs path when lib reported as .tbd"
+        assert !lib.end_with?(".dylib"), "expected abs path when lib reported as .dylib"
+      else
+        p = Pathname.new(lib)
+        if p.extname == ".tbd" || p.extname == ".dylib"
+          assert p.absolute?, "expected abs path when lib reported as .tbd or .dylib"
+        end
+      end
+    end
+  end
+end
