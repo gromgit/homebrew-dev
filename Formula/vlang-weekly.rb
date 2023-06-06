8d71e1183a6eda19b00770ca8f408db8473d0ded
Adrian Ho <aho+bruteforce@03s.net>
2023-06-06 23:16:46 +0800

    vlang-weekly: update 2023.23 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -1,8 +1,8 @@
 class VlangWeekly < Formula
   desc "V programming language"
   homepage "https://vlang.io"
-  url "https://github.com/vlang/v/archive/weekly.2023.22.tar.gz"
-  sha256 "bb53a0e5ff0c754df8cda4fec37a302acde1e6f5459b071cf4a1c109bfbd0a8f"
+  url "https://github.com/vlang/v/archive/weekly.2023.23.tar.gz"
+  sha256 "bcd1d5d0eb7c398bfa8f1827835f56a6741237134cc09b3d0dc181e495a4c907"
   license "MIT"
 
   livecheck do
@@ -11,10 +11,8 @@ class VlangWeekly < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2023.22"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d33bc6d71d07a78612128477d6bfbd7cd3a189ea1eb5f33e01a8db5f9554359f"
-    sha256 cellar: :any_skip_relocation, monterey:       "becd135d9520e870854b5cad19dc8563e2327ba2aad90278f55e2b63fa1b0531"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e09676479c59ac16585afbeba8ef09be51a62fbb2af0d39281043035f2be95ba"
+    root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2023.23"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0ab6b70ab6b1ed87099dc4467852bba6abecb29c8996def7d8c7c85edc4d9a75"
   end
 
   conflicts_with "vlang", because: "both install `v` binaries"
