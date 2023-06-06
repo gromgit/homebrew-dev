2983058f4ea30bbf0ffae116065fc06d63c3ce8e
Adrian Ho <the.gromgit@gmail.com>
2023-06-06 15:21:09 +0000

    vlang-weekly: update 2023.23 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -13,6 +13,7 @@ class VlangWeekly < Formula
   bottle do
     root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2023.23"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0ab6b70ab6b1ed87099dc4467852bba6abecb29c8996def7d8c7c85edc4d9a75"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "998c70e586922f8e30b39b13d9fa1f554524dfb406b486c768ced25c0e78e801"
   end
 
   conflicts_with "vlang", because: "both install `v` binaries"
