45fb9e0a3009500349359a5ee0601b39817deb6d
Adrian Ho <aho+rover-air61.03s.net>
2024-05-28 00:33:19 +0800

    vlang-weekly: update 2024.22 bottle.

--- Formula/vlang-weekly.rb
+++ Formula/vlang-weekly.rb
@@ -1,8 +1,8 @@
 class VlangWeekly < Formula
   desc "V programming language"
   homepage "https://vlang.io"
-  url "https://github.com/vlang/v/archive/weekly.2023.23.tar.gz"
-  sha256 "bcd1d5d0eb7c398bfa8f1827835f56a6741237134cc09b3d0dc181e495a4c907"
+  url "https://github.com/vlang/v/archive/refs/tags/weekly.2024.22.tar.gz"
+  sha256 "dd32d36dd85758a1f64fbbad8c9d42ef6384ed4e0a4e786fc170268bcd370975"
   license "MIT"
 
   livecheck do
@@ -11,16 +11,37 @@ class VlangWeekly < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2023.23"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0ab6b70ab6b1ed87099dc4467852bba6abecb29c8996def7d8c7c85edc4d9a75"
-    sha256 cellar: :any_skip_relocation, monterey:       "c090ae3b7de9a4c88844f9949cdecb690fd6fb8268a1ec65e0c28a20e57f6315"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "998c70e586922f8e30b39b13d9fa1f554524dfb406b486c768ced25c0e78e801"
+    root_url "https://github.com/gromgit/homebrew-dev/releases/download/vlang-weekly-2024.22"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "6db7d691e38bebb4777f0d3930189561e515a7e6cd872db0d785fead9c9f39de"
   end
 
   conflicts_with "vlang", because: "both install `v` binaries"
 
+  depends_on "bdw-gc"
+
+  # upstream discussion, https://github.com/vlang/v/issues/16776
+  # macport patch commit, https://github.com/macports/macports-ports/commit/b3e0742a
+  patch :DATA
+
   def install
+    inreplace "vlib/builtin/builtin_d_gcboehm.c.v", "@PREFIX@", Formula["bdw-gc"].opt_prefix
+    %w[up self].each { |cmd|
+      (buildpath/"cmd/tools/v#{cmd}.v").delete
+      (buildpath/"cmd/tools/v#{cmd}.v").write <<~EOS
+        println('ERROR: `v #{cmd}` is disabled. Use `brew upgrade #{name}` to update V.')
+      EOS
+    }
+
     system "make"
+    system "./v", "-prod", "-o", "v", "cmd/v"
+    system "./v", "-prod", "build-tools"
+
+    # clean up unnecessary stuff
+    %w[thirdparty/tcc/.git].each { |d| (buildpath/d).rmtree }
+    (buildpath/"cmd/tools/.disable_autorecompilation").write <<~EOS
+      This is just a placeholder to tell V not to recompile any of its own tools.
+    EOS
+
     libexec.install "cmd", "thirdparty", "v", "v.mod", "vlib"
     bin.install_symlink libexec/"v"
     pkgshare.install "examples", "tutorials"
@@ -33,3 +54,25 @@ class VlangWeekly < Formula
     assert_equal "Hello, World!", shell_output("./test").chomp
   end
 end
+
+__END__
+diff --git a/vlib/builtin/builtin_d_gcboehm.c.v b/vlib/builtin/builtin_d_gcboehm.c.v
+index 0a13b64..23fca2b 100644
+--- a/vlib/builtin/builtin_d_gcboehm.c.v
++++ b/vlib/builtin/builtin_d_gcboehm.c.v
+@@ -31,12 +31,12 @@ $if dynamic_boehm ? {
+ } $else {
+ 	$if macos || linux {
+ 		#flag -DGC_BUILTIN_ATOMIC=1
+-		#flag -I @VEXEROOT/thirdparty/libgc/include
+-		$if (prod && !tinyc && !debug) || !(amd64 || arm64 || i386 || arm32) {
++		#flag -I @PREFIX@/include
++		$if (!macos && prod && !tinyc && !debug) || !(amd64 || arm64 || i386 || arm32) {
+ 			// TODO: replace the architecture check with a `!$exists("@VEXEROOT/thirdparty/tcc/lib/libgc.a")` comptime call
+ 			#flag @VEXEROOT/thirdparty/libgc/gc.o
+ 		} $else {
+-			#flag @VEXEROOT/thirdparty/tcc/lib/libgc.a
++			#flag @PREFIX@/lib/libgc.a
+ 		}
+ 		$if macos {
+ 			#flag -DMPROTECT_VDB=1
