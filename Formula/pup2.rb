c8e17526ed70a79365356b49594f0e30889e1014
Adrian Ho <the.gromgit@gmail.com>
2024-05-28 00:09:42 +0800

    pup2: update 0.4.1_1 bottle.

--- Formula/pup2.rb
+++ Formula/pup2.rb
@@ -11,6 +11,7 @@ class Pup2 < Formula
     root_url "https://github.com/gromgit/homebrew-dev/releases/download/pup2-0.4.1"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a6d08531683f2a4a6d091b4a1cded0ece435346fe6ff6689f081e2d2fdd8cfac"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "df4ecec8938945ae5205129c1b0bd9854bee2c66afba64712df3e3254160146c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd6109ec13a31c1443a96a4d2235df3200047e1ce18420f9bdb578c79a2dde81"
   end
 
   depends_on "go" => :build
