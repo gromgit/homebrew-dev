8d7090aaa5989b24d56bfd6fc50a13184cd35fdb
Adrian Ho <the.gromgit@gmail.com>
2025-02-21 13:04:51 +0800

    nuitka 2.6.6
    Closes #16.
    
    Signed-off-by: Adrian Ho <215702+gromgit@users.noreply.github.com>

--- Formula/nuitka.rb
+++ Formula/nuitka.rb
@@ -3,8 +3,8 @@ class Nuitka < Formula
 
   desc "Python compiler written in Python"
   homepage "https://nuitka.net"
-  url "https://github.com/Nuitka/Nuitka/archive/refs/tags/1.1.4.tar.gz"
-  sha256 "619f93eff88e7ff3ecef6a13ef8224085bbc588b7a3cc0a4704d7807c29884c0"
+  url "https://github.com/Nuitka/Nuitka/archive/refs/tags/2.6.6.tar.gz"
+  sha256 "dde904513d944b75ad06327a513e9f47a74914daa7a32d8470f99c6f7071a58e"
   license "Apache-2.0"
 
   bottle do
@@ -16,11 +16,23 @@ class Nuitka < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "6fec6ff79dd6a11eaf169028cdad0931cdac458f72368a5aca2ea512d23ee607"
   end
 
-  depends_on "llvm"
-  depends_on "python@3.9"
+  depends_on "ccache"
+  depends_on "python@3.13"
+
+  resource "ordered-set" do
+    url "https://files.pythonhosted.org/packages/4c/ca/bfac8bc689799bcca4157e0e0ced07e70ce125193fc2e166d2e685b7e2fe/ordered-set-4.1.0.tar.gz"
+    sha256 "694a8e44c87657c59292ede72891eb91d34131f6531463aab3009191c77364a8"
+  end
+
+  resource "zstandard" do
+    url "https://files.pythonhosted.org/packages/ed/f6/2ac0287b442160a89d726b17a9184a4c615bb5237db763791a7fd16d9df1/zstandard-0.23.0.tar.gz"
+    sha256 "b2d8c62d08e7255f68f7a740bae85b3c9b8e5466baa9cbf7f57f1cde0ac6bc09"
+  end
 
   def install
     virtualenv_install_with_resources
+    man1.install Dir["doc/*.1"]
+    (share/"doc/#{name}").install Dir["*.rst"]
   end
 
   test do
@@ -35,7 +47,7 @@ class Nuitka < Formula
           main()
     EOS
     assert_match "Talk Hello World", shell_output("python3 test.py")
-    system "#{bin}/nuitka3", "-o", "test", "test.py"
+    system bin/"nuitka", "--onefile", "-o", "test", "test.py"
     assert_match "Talk Hello World", shell_output("./test")
     ohai "test = #{File.size("test")} bytes"
     ohai shell_output("/bin/ls -lR >&2")
