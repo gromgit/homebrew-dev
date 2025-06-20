400b21dc3671e0726d2e5eaa10d6f7071c7d3115
Adrian Ho <the.gromgit@gmail.com>
2025-06-20 13:50:19 +0800

    fry 4.1.3: new formula
    Closes #45.
    
    Signed-off-by: Adrian Ho <215702+gromgit@users.noreply.github.com>

---
+++ Formula/fry.rb
@@ -0,0 +1,46 @@
+class Fry < Formula
+  desc "FatScript interpreter"
+  homepage "https://fatscript.org/en/"
+  url "https://gitlab.com/fatscript/fry/-/archive/v4.1.3/fry-v4.1.3.tar.bz2"
+  sha256 "5683308fc798b72aca9ddda62ae53e154c05162b9ade85adadd9360cf01afead"
+  license "GPL-3.0-only"
+
+  depends_on "curl"
+  depends_on "libffi"
+  depends_on "openssl@3"
+  depends_on "readline"
+
+  patch :DATA
+
+  def install
+    ENV["CONT_IMG_VER"] = version
+    ENV["useNCurses"] = "1"
+    system "./compile.sh"
+    bin.install "bin/fry"
+    man1.install "man/fry.1"
+    pkgshare.install "benchmark", "extras", "sample", "test"
+  end
+
+  test do
+    assert_match "current time in Beijing", shell_output("#{bin}/fry #{pkgshare}/sample/timezones.fat")
+  end
+end
+
+__END__
+diff --git a/compile.sh b/compile.sh
+index b9ae10e..ae32fb9 100755
+--- a/compile.sh
++++ b/compile.sh
+@@ -11,9 +11,9 @@
+ # Licensed under the GNU General Public License v3.0.
+ # See LICENSE file in the project root for full license.
+ 
+-useNCurses=0   # set to 1 for ncurses, 0 for fatcurses
++useNCurses=${useNCurses:-0}   # set to 1 for ncurses, 0 for fatcurses
+ 
+-if git version &>/dev/null
++if [ -d .git ] && git version &>/dev/null
+ then
+     FRY_VERSION="$(git describe --abbrev=6 --dirty --always --tags)"
+ elif [ -z "$CONT_IMG_VER" ]
+
