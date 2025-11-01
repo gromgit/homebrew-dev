71269053c16c56aafe0f04707b9365256bbfbd99
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-11-01 14:25:20 +0000

    uxn: add 1.0 bottle.

--- Formula/uxn.rb
+++ Formula/uxn.rb
@@ -6,6 +6,14 @@ class Uxn < Formula
   license "MIT"
   head "https://git.sr.ht/~rabbits/uxn", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/gromgit/dev"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "89678a145fe0da203c1d5aa63d60afe32cb886d794582a5b160baab3a9feb898"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "715e6d419cacb0a19ac3a597b2262b79adf8d32d5e3021e2651b87408e133e08"
+    sha256 cellar: :any_skip_relocation, ventura:       "13d3b543fdba4a494773137a737266f0dbd7e079c361d56b510706a85faccc0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9a27b4b9e57c007661ff0855313ddbb52103bc53fa7f4d77c7addbf5e2555e34"
+  end
+
   depends_on "sdl2"
 
   def install
