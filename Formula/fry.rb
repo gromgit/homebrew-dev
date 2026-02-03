f4f9529c0eb28fe1ad91b19b1004ba2e882f7ac9
Adrian Ho <215702+gromgit@users.noreply.github.com>
2026-02-03 12:38:15 +0000

    fry: update 4.4.0 bottle.

--- Formula/fry.rb
+++ Formula/fry.rb
@@ -9,9 +9,10 @@ class Fry < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/gromgit/dev"
-    sha256 cellar: :any,                 arm64_sequoia: "6b2cc9257e9266bf31566d7c56442bdfe2ead7b373e88f237a255510fa926573"
-    sha256 cellar: :any,                 arm64_sonoma:  "f1f6b80d0e488bbf14bdbe0ca363befa8ef48abafa5a95dedc9da6d8546ee157"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "138015d7c3a3a55511f43cfc93e33731abd93610a0f869b60797020ff99d083f"
+    sha256 cellar: :any,                 arm64_tahoe:   "9754454cd4f4551fe8fbeb9c2db78e5ebc68c20a50be55b77775c72dc360b4af"
+    sha256 cellar: :any,                 arm64_sequoia: "0cd5a75936115037b8c185bd9b94a65a0599ce6e6fb4ff90cf1d1ef8cfa5bed0"
+    sha256 cellar: :any,                 arm64_sonoma:  "3603cceca2706b606320e7c474e936f6b585815bd780b5c14df9279abd5ac7bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "29640ed12308660ab3c9b6b47bbf08a824472695159deb2e6acb5f618d74da65"
   end
 
   depends_on "curl"
